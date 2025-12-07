e6e827a8d483281580034b93bf7c13acda327eac
Kireev Mikle <kireev.mikle@gmail.com>
2025-12-07 12:29:28 +0000

    simple_models

---
+++ app/models.py
@@ -0,0 +1,42 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.text
+
+
+class UserAnswer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.user.username} - {self.question.text} - {self.answer.text}"
+
+
